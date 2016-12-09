a48fecdf886ffe330ce87a389c4efaddfec7d168
RJRowland93 <r.rowland93@gmail.com>
2016-12-09 09:17:52 -0600

    fleshed out the html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,26 @@
 var friends = [{
 	name: "Dogbert",
 	photo: "https://www.what-dog.net/Images/faces2/main001.jpg", 
-	scores: []
+	scores: ["1", "3", "5"]
 }, {
 	name: "Billy",
 	photo: "https://www.what-dog.net/Images/faces2/main000.jpg", 
-	scores: []
+	scores: ["2", "4", "5"]
 }, 
 {
 	name: "Slappy",
 	photo: "https://www.what-dog.net/Images/faces2/scroll0015.jpg", 
-	scores: []
+	scores: ["4", "1", "5"]
 }, 
 {
 	name: "Griffon",
 	photo: "https://www.what-dog.net/Images/faces2/main005.jpg", 
-	scores: []
+	scores: ["2", "2", "5"]
 }, 
 {
 	name: "Dooby",
 	photo: "https://www.what-dog.net/Images/faces2/main002.jpg", 
-	scores: []
+	scores: ["5", "5", "5"]
 }, ];
 
 module.exports = friends;
