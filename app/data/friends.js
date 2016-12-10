2aa53f128196c5ba206fbb1ea70696da08e3565d
RJRowland93 <r.rowland93@gmail.com>
2016-12-10 10:55:47 -0600

    began logic for survey match results

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,30 +2,30 @@ var friends = [{
 	route: "dogbert",
 	name: "Dogbert",
 	photo: "https://www.what-dog.net/Images/faces2/main001.jpg", 
-	scores: ["1", "3", "5"]
+	scores: [1, 3, 5]
 }, {
 	route: "billy",
 	name: "Billy",
 	photo: "https://www.what-dog.net/Images/faces2/main000.jpg", 
-	scores: ["2", "4", "5"]
+	scores: [2, 4, 5]
 }, 
 {
 	route: "slappy",
 	name: "Slappy",
 	photo: "https://www.what-dog.net/Images/faces2/scroll0015.jpg", 
-	scores: ["4", "1", "5"]
+	scores: [4, 1, 5]
 }, 
 {
 	route: "griffon",
 	name: "Griffon",
 	photo: "https://www.what-dog.net/Images/faces2/main005.jpg", 
-	scores: ["2", "2", "5"]
+	scores: [2, 2, 5]
 }, 
 {
 	route: "dooby",
 	name: "Dooby",
 	photo: "https://www.what-dog.net/Images/faces2/main002.jpg", 
-	scores: ["5", "5", "5"]
+	scores: [5, 5, 5]
 }
 ];
 
