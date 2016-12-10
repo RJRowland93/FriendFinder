0b4a33e8209ddb7a46e6a417154ed7207d09842f
RJRowland93 <r.rowland93@gmail.com>
2016-12-10 09:39:34 -0600

    updated api routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,28 @@
 var friends = [{
+	route: "dogbert",
 	name: "Dogbert",
 	photo: "https://www.what-dog.net/Images/faces2/main001.jpg", 
 	scores: ["1", "3", "5"]
 }, {
+	route: "billy",
 	name: "Billy",
 	photo: "https://www.what-dog.net/Images/faces2/main000.jpg", 
 	scores: ["2", "4", "5"]
 }, 
 {
+	route: "slappy",
 	name: "Slappy",
 	photo: "https://www.what-dog.net/Images/faces2/scroll0015.jpg", 
 	scores: ["4", "1", "5"]
 }, 
 {
+	route: "griffon",
 	name: "Griffon",
 	photo: "https://www.what-dog.net/Images/faces2/main005.jpg", 
 	scores: ["2", "2", "5"]
 }, 
 {
+	route: "dooby",
 	name: "Dooby",
 	photo: "https://www.what-dog.net/Images/faces2/main002.jpg", 
 	scores: ["5", "5", "5"]
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,12 +1,32 @@
 var friends = require("../data/friends.js");
 
 module.exports = function(app) {
-	app.get("/api/friends", function (req, res) {
-		res.json(friends);
+	app.get("/api/friends/:characters?", function (req, res) {
+		var chosen = req.params.characters;
+
+		if (chosen) {
+			console.log(chosen);
+			for(var i = 0; i < friends.length; i++) {
+				if (chosen === friends[i].route) {
+					res.json(friends[i]);
+				}
+			}
+			res.json(false);
+		} else {
+			res.json(friends);
+		}
 	}); //end get
 
-	app.post("/api/friends", function(req, res) {
-		friends.push(req.body);
+	app.post("/api/newfriend", function(req, res) {
+		var newfriend = req.body;
+		newfriend.route = newfriend.name.replace(/\s+/g, "").toLowerCase();
+		// change new friend scores to numbers
+		for (var i = 0; i < newfriend.scores.length; i++) {
+			newfriend.scores[i] = parseInt(newfriend.scores[i]);
+		}
+		friends.push(newfriend);
+		res.json(newfriend);
 	}); //end post
-}
+
+};
 
